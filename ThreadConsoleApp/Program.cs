4cbe470c33542178913b9da2339ff36f4e214b24
Georgi Yanakiev <georgiyanakiev@hopsoftware.com>
2019-12-29 16:32:14 +0200

    Thread isn't catched by Exception

--- ThreadConsoleApp/Program.cs
+++ ThreadConsoleApp/Program.cs
@@ -11,12 +11,29 @@ namespace ThreadConsoleApp
     {   
         public static void CallNewThread()
         {
-            Console.WriteLine("A new child thread will start right now...");
 
-            int sleeping = 3000; // the thread will pause for 3 seconds
-            Console.WriteLine("Child Thread will pause for {0} seconds", sleeping / 1000);
-            Thread.Sleep(sleeping);
-            Console.WriteLine("Now the thread is back from sleeping.");
+            try
+            {
+                Console.WriteLine("A new child thread will start right now...");
+                for (int i = 0; i > 10; i++) { 
+                    Thread.Sleep(1000);
+                Console.WriteLine(i);
+                }
+                Console.WriteLine("Child Thread is finally completed.");
+            }
+            catch(ThreadAbortException e )
+            {
+                Console.WriteLine("Thread Abort Exception.");
+            }
+            finally
+            {
+                Console.WriteLine("Thread isn't catched by Exception");
+            }
+
+            //int sleeping = 3000; // the thread will pause for 3 seconds
+            //Console.WriteLine("Child Thread will pause for {0} seconds", sleeping / 1000);
+            //Thread.Sleep(sleeping);
+            //Console.WriteLine("Now the thread is back from sleeping.");
 
         }
             
